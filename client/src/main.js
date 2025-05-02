6792f239c629dc9be388813f256d23670a6b7cec
Lennart Lemmens <lennartlemmens@gmail.com>
2025-05-02 16:59:48 +0200

    Search button now reactivates even when there's an error.

--- client/src/main.js
+++ client/src/main.js
@@ -15,38 +15,32 @@ const genre = document.getElementById("genre");
 getListData("platforms", "fields name; sort name asc; limit 50; where generation > 7;", platform);
 getListData("genres", "fields name; sort name asc; limit 25;", genre);
 
-/*
-const game = new Game(1, "test", "test", "test", "test");
-game.toggleFavorite();
-*/
-
 searchButton.addEventListener("click", () => {
     searchButton.textContent = "Loading...";
     searchButton.setAttribute("disabled", "");
     resultList.textContent = "Loading...";
 
     let filters = "where ";
-
     filters += platform.value ? `platforms.name = ("${platform.value}") & ` : "";
     filters += genre.value ? `genres.name = ("${genre.value}") & ` : "";
     filters = filters.substring(0, filters.length-3);
     filters += ";";
-
     if (filters === "whe;") filters = "";
-    console.log(filters);
 
     requestData("games", `
           fields name, cover.image_id, genres.name, multiplayer_modes.*, platforms.name;
-          limit 10;
+          limit 60;
           ${filters}
       `)
     .then(data => {
         generateResultList(data);
-        searchButton.textContent = "Search";
-        searchButton.removeAttribute("disabled");
     })
     .catch(error => {
         resultList.textContent = error;
         console.error(error);
+    })
+    .finally(() => {
+        searchButton.textContent = "Search";
+        searchButton.removeAttribute("disabled");
     });
 });
