7800049fbdaa68e3e8d9e553050331b1f79a1f05
Lennart Lemmens <lennartlemmens@gmail.com>
2025-05-03 11:08:54 +0200

    Styling: changed favorite icon color.

--- client/src/constants/favoriteIcon.js
+++ client/src/constants/favoriteIcon.js
@@ -1,5 +1,5 @@
 export const favoriteIconFull = `
-            <svg fill="#DBE2EF" height="25px" width="25px" version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" 
+            <svg fill="#112D4E" height="25px" width="25px" version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" 
                 viewBox="0 0 482.207 482.207" xml:space="preserve">
             <polygon points="482.207,186.973 322.508,153.269 241.104,11.803 159.699,153.269 0,186.973 109.388,308.108 92.094,470.404 
                 241.104,403.803 390.113,470.404 372.818,308.108 "/>
@@ -7,7 +7,7 @@ export const favoriteIconFull = `
             `;
 
 export const favoriteIconEmpty = `
-            <svg fill="#DBE2EF" height="25px" width="25px" version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" 
+            <svg fill="#112D4E" height="25px" width="25px" version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" 
                 viewBox="0 0 482.207 482.207" xml:space="preserve">
             <path d="M482.207,186.973l-159.699-33.705L241.104,11.803l-81.404,141.465L0,186.973l109.388,121.134L92.094,470.404l149.01-66.6
                 l149.01,66.6l-17.294-162.296L482.207,186.973z M241.104,370.943l-113.654,50.798l13.191-123.788l-83.433-92.393l121.807-25.707
